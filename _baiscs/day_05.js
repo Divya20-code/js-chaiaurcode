2a2d3fa1d4e98ee7dc4b3eae6b7457853d6896c4
D <divyadd.0193@gmail.com>
2026-09-04 03:10:37 +0000

    dataypes

---
+++ _baiscs/day_05.js
@@ -0,0 +1,35 @@
+
+
+//"use strict";// treat all js code as a newer version 
+
+//alert(3+3) // we are usinf node js not browser 
+
+//datatypes
+
+ /* number == ek range hoti 2 power 53  
+ bigint  bigger number 
+
+ string = " "
+ boolean = true/false
+
+ null = standalone value 
+
+ undefined 
+
+ symbol = uniques 
+
+ object 
+
+
+ 
+ 
+ */
+
+
+ console.log(typeof undefined);//undefiend 
+ 
+
+
+ console.log(typeof null);//object 
+ 
+ 
