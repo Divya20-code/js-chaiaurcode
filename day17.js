4450e3f2db16d64c87de269a3df67720724a7fff
Divya20-code <divyadd.0193@gmail.com>
2026-09-17 07:22:28 +0530

    object ka advance

---
+++ day17.js
@@ -0,0 +1,51 @@
+/* 
+
+const tinderUser = {}
+
+tinderUser.name = "John Doe"
+tinderUser.age = 25
+tinderUser.isLoggedIn = true
+
+
+console.log(tinderUser);
+
+
+const regularUser = {
+    email: " somegamil.com",
+    fullName: {
+        userFullName: {
+            firstName: "John",
+            lastName: "Doe"
+        }
+    }
+}
+
+console.log(regularUser.fullName.userFullName.firstName); */
+
+const obj1 = {
+    key1: "value1",
+    key2: "value2"
+}
+const obj2 = {
+    key1: "value3",
+    key2: "value4"
+}
+const obj3 = {
+    key1: "value4",
+    key2: "value5"
+}
+
+const newObject =  (obj1)
+console.log(newObject)
+
+ {
+    "name": "hitesh",
+     "coursename": "js in hindi",
+    "price": "free"
+ }
+
+[
+    {},
+    {},
+    {}
+]
